cf90f5e789b2201dcc7ac893c90b95245e8c156b
Randy Terbush <randy@terbush.org>
2019-05-16 16:23:21 -0600

    Fix SQL syntax

--- salt/roles/database/files/init-salt-queues.sql
+++ salt/roles/database/files/init-salt-queues.sql
@@ -5,7 +5,7 @@ CREATE DATABASE salt WITH ENCODING 'utf-8';
 -- Table structure for table `salt`
 --
 DROP TABLE IF EXISTS salt;
-CREATE OR REPLACE TABLE salt(
+CREATE TABLE salt(
    id SERIAL PRIMARY KEY,
    data jsonb NOT NULL
 );
