b111495ebcfd11a5c56171c1f0df8ac168e60c12
Randy Terbush <randy@terbush.org>
2019-05-17 14:36:06 -0600

    Fix notepad test script

--- salt/tasks/scripts/test-job-run.py
+++ salt/tasks/scripts/test-job-run.py
@@ -34,47 +34,25 @@ def run_notepad():
 
     app.Notepad.menu_select("File->PageSetup")
 
-    # ----- Page Setup Dialog ----
-    # Select the 4th combobox item
-    app.PageSetupDlg.SizeComboBox.select(4)
-
-    # Select the 'Letter' combobox item or the Letter
-    try:
-        app.PageSetupDlg.SizeComboBox.select("Letter")
-    except ValueError:
-        app.PageSetupDlg.SizeComboBox.select('Letter (8.5" x 11")')
-
-    app.PageSetupDlg.SizeComboBox.select(2)
-
     # type some text - note that extended characters ARE allowed
     app.Notepad.Edit.set_edit_text("I am typing s\xe4me text to Notepad\r\n\r\n"
         "And then I am going to quit")
 
-    app.Notepad.Edit.right_click()
-    app.Popup.menu_item("Right To Left Reading Order").click()
-
     # Try and save
     app.Notepad.menu_select("File->SaveAs")
     app.SaveAs.EncodingComboBox.select("UTF-8")
-    app.SaveAs.FileNameEdit.set_edit_text("Example-utf8.txt")
+    app.SaveAs.Edit.set_edit_text("example-utf8.txt")
     app.SaveAs.Save.close_click()
 
     # while the dialog exists wait upto 30 seconds (and yes it can
     # take that long on my computer sometimes :-( )
     app.SaveAsDialog2.Cancel.wait_not('enabled')
-
-    # If file exists - it asks you if you want to overwrite
-    try:
-        app.SaveAs.Yes.wait('exists').close_click()
-    except MatchError:
-        print('Skip overwriting...')
+    app.SaveAs.Yes.wait('exists').close_click()
 
     time.sleep(20)
 
     # exit notepad
     app.Notepad.menu_select("File->Exit")
-
-    print("That took %.3f to run"% (time.time() - start))
 
 if __name__ == "__main__":
     run_notepad()
