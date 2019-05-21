2db28eb120b2a75f36f58226b4fd6c8b948d2e23
Randy Terbush <randy@terbush.org>
2019-05-21 12:41:58 -0600

    Needs to be salt_queues

--- salt/roles/database/files/init-salt-queues.sql
+++ salt/roles/database/files/init-salt-queues.sql
@@ -1,8 +1,8 @@
-DROP DATABASE IF EXISTS salt-queues;
-CREATE DATABASE salt-queues WITH ENCODING 'utf-8' OWNER salt;
-GRANT ALL PRIVILEGES ON DATABASE salt-queues TO salt;
-\connect salt-queues;
-CREATE SCHEMA IF NOT EXISTS salt-queues AUTHORIZATION salt;
+DROP DATABASE IF EXISTS salt_queues;
+CREATE DATABASE salt_queues WITH ENCODING 'utf-8' OWNER salt;
+GRANT ALL PRIVILEGES ON DATABASE salt_queues TO salt;
+\connect salt_queues;
+CREATE SCHEMA IF NOT EXISTS salt_queues AUTHORIZATION salt;
 
 --
 -- Table structure for table `salt`
