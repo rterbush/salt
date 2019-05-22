1695143ac2b949cbf0ee30d465c1801426647e06
Randy Terbush <randy@terbush.org>
2019-05-22 06:39:08 -0600

    Change up database initialization to create separate database for pgjsonb queue.

--- salt/roles/database/files/init-salt-queues.sql
+++ salt/roles/database/files/init-salt-queues.sql
@@ -7,10 +7,13 @@ CREATE SCHEMA IF NOT EXISTS salt_queues AUTHORIZATION salt;
 --
 -- Table structure for table `salt`
 --
-DROP TABLE IF EXISTS salt;
-CREATE OR REPLACE TABLE salt(
+DROP TABLE IF EXISTS salt_queues(winjob);
+CREATE OR REPLACE TABLE salt_queues(winjob) (
    id SERIAL PRIMARY KEY,
    data jsonb NOT NULL
 );
+
+ALTER TABLE salt_queues(winjob)
+    ADD CONSTRAINT unique_data UNIQUE salt_queues(data);
 
 GRANT ALL PRIVILEGES ON TABLE salt TO salt;
